e441eeda40a4ef7e57bf531f6a681026daf621f9
karan059 <karansinghmanhas447@gmail.com>
2026-01-03 18:59:16 +0530

    first commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+	return (
+		<div className="min-h-screen flex items-center justify-center">
+			<h1 className="text-2xl">404 — Page Not Found</h1>
+		</div>
+	);
+};
+
+export default NotFound;
+
